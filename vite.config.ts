c2e07220d843eec31e550e7305529d0faee82aab
JamesAthanasiou <james.athanasiou@live.com>
2024-05-06 23:05:49 -0400

    Add docker stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Server is for docker
+  server: {
+    host: true,
+    port: 8000,
+  }
 })
