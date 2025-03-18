a6b809e519b1a3477e5afabdf0188670eced9720
JamesAthanasiou <james.athanasiou@live.com>
2025-03-17 23:17:34 -0400

    Format. Remove comment.

--- src/routes/person.index.tsx
+++ src/routes/person.index.tsx
@@ -9,60 +9,44 @@ export const Route = createFileRoute('/person/')({
   component: PersonManagement,
 })
 
-// // TOOD add protected route
-// export const Route = createFileRoute('/person/')({
-//   component: Friends,
-//   // TODO apply to all other protected routes. Create main protected route?
-//   beforeLoad: async ({ location }) => {
-//     if (!isAuthenticated()) {
-//       throw redirect({
-//         to: '/login',
-//         search: {
-//           redirect: location.href,
-//         },
-//       })
-//     }
-//   },
-// })
-
 function PersonManagement() {
-  const [persons, setPersons] = useState<Person[]>([]);
+	const [persons, setPersons] = useState<Person[]>([]);
 
-  useEffect(() => {
-      let active = true;
+	useEffect(() => {
+		let active = true;
 
-      const fetchAllPersons = async () => {
-          const data = await (getAllPersons()) as Person[];
-          if (active) {
-              setPersons(data);
-          }
-      }
+		const fetchAllPersons = async () => {
+			const data = await (getAllPersons()) as Person[];
+			if (active) {
+				setPersons(data);
+			}
+		}
 
-      fetchAllPersons();
-      return () => {
-          active = false;
-      };
-  },[]);
+		fetchAllPersons();
+		return () => {
+			active = false;
+		};
+	},[]);
 
-  const personsListUpdated = async() => {
-    const newPersons = await (getAllPersons()) as Person[];
-    setPersons(newPersons);
-  }
+	const personsListUpdated = async() => {
+		const newPersons = await (getAllPersons()) as Person[];
+		setPersons(newPersons);
+	}
 
-  return (
-    <div>
-      <div className="section-container">
-        <div>People</div>
-        <AllPersons persons={persons}/>
-      </div>
-      <div className="section-container">
-        <div>Add a person</div>
-        <PersonForm updateParent={personsListUpdated}/>
-      </div>
-      <div className="section-container">
-        <div className="p-2">Remove a person</div>
-        {/* TODO dropdown */}
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<div className="section-container">
+				<div>People</div>
+				<AllPersons persons={persons}/>
+			</div>
+			<div className="section-container">
+				<div>Add a person</div>
+				<PersonForm updateParent={personsListUpdated}/>
+			</div>
+			<div className="section-container">
+				<div className="p-2">Remove a person</div>
+				{/* TODO dropdown */}
+			</div>
+		</div>
+	)
 }
